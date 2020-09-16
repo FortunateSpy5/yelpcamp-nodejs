ef2b0b0a2a2543a78b37e28bd2853cece7732c79
Soumotanu Mazumdar <msoumotanu@gmail.com>
2020-09-16 18:07:43 +0530

    Basic routes and html

--- index.js
+++ index.js
@@ -5,7 +5,30 @@ const port = 3000;
 app.set("view engine", "ejs");
 
 app.get('/', (req, res) => {
-    res.render('landing');
+    res.render('landing', {
+        title: "YelpCamp"
+    });
+});
+
+app.get('/campgrounds', (req, res) => {
+    let campgrounds = [
+        {
+            name: "Salmon Creek",
+            image: "https://images.unsplash.com/photo-1492648272180-61e45a8d98a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+        },
+        {
+            name: "Granite Hill",
+            image: "https://images.unsplash.com/photo-1471115853179-bb1d604434e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=959&q=80"
+        },
+        {
+            name: "Mountain Goat's Rest",
+            image: "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+        }
+    ]
+    res.render('campgrounds', {
+        title: "Campgrounds",
+        campgrounds: campgrounds
+    });
 });
 
 
